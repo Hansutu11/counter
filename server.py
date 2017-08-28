5cb99c1d9ff2271c0f62f136223e936d953c1473
Hans Utu <utuhans@gmail.com>
2017-08-27 18:08:16 -0700

    finished counter

---
+++ server.py
@@ -0,0 +1,13 @@
+from flask import Flask, session, render_template
+app = Flask(__name__)
+app.secret_key = 'secretsecret'
+
+@app.route('/')
+def index():
+    if 'counter' not in session:
+        session['counter'] = 0
+    session ['counter'] += 1
+    return render_template('index.html')
+
+
+app.run(debug=True)
